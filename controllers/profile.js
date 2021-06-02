d8780078bf25f194bf80650cf4846c121886d3d0
sfazli96 <sfazli96@gmail.com>
2021-06-01 21:05:40 -0700

    working on profile page

---
+++ controllers/profile.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+function getProfile(req, res) {
+    res.sendFile(path.join(__dirname, '/test/', '../public/profile.html'));
+
+    res.render('profile', { 'username': req.params.username});
+}
+
+module.exports = {
+    getProfile
+}
