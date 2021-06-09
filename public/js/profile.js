94f110c3038a25ef1c0b74b959a9b973c77d6ccd
Danz Moses <danzmoses@gmail.com>
2021-06-09 13:18:06 -0700

    Fixed profile page to correctly show the user's information

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,54 +2,45 @@ console.log('in profilejs')
 
 // determine if the current user is visiting their own profile
 auth.onAuthStateChanged(user => {
-    if (user) {
-        const doc = db1.collection('users').doc(user.uid);
-        doc.get().then((obj) => {
-            if (!obj.exists) {
-                console.log('no doc exists') 
-            }
-            else {
-                document.getElementById("firstName").innerHTML = obj.data().firstName;
-                document.getElementById("lastName").innerHTML = obj.data().lastName;
-                document.getElementById("location").innerHTML = obj.data().location;
-                document.getElementById("favoriteGenre").innerHTML = obj.data().favoriteGenre;
-                document.getElementById("biography").innerHTML = obj.data().biography;
-            }
+    const url = window.location.href;
+    let username = url.substr(url.lastIndexOf("/") + 1);
+    const doc = db1.collection('users').where('username', '==', username);
+    doc.get().then((snapshot) => {
+        snapshot.forEach(doc => {
+            document.getElementById("firstName").innerHTML = doc.data().firstName;
+            document.getElementById("lastName").innerHTML = doc.data().lastName;
+            document.getElementById("location").innerHTML = doc.data().location;
+            document.getElementById("favoriteGenre").innerHTML = doc.data().favoriteGenre;
+            document.getElementById("biography").innerHTML = doc.data().biography;     
         })
-    }
-    else {
-        console.log('no user logged in')
-    }
-
-    // const doc = db1.collection('users').doc(user.uid);
-    // doc.get().then((obj) => {
-    //     console.log(obj.data());
-    // })
-
-    const email = "johndoe@email.com"
-    const password = "test123"
-    const username = "johndoe"
-    const firstName = "John"
-    const lastName = "Doe"
-    const biography = "This is a sample biography."
-    const location = "United States"
-    const favoriteGenre = "Action"
-
-    // auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    //     return db1.collection('users').doc(cred.user.uid).set({
-    //         username: username,
-    //         firstName: firstName,
-    //         lastName: lastName,
-    //         biography: biography,
-    //         location: location,
-    //         favoriteGenre: favoriteGenre,
-    //     });
-    //   }).then(() => {
-    //     // // close the signup modal & reset form
-    //     // const modal = document.querySelector('#modal-signup');
-    //     // M.Modal.getInstance(modal).close();
-    //     // signupForm.reset();
-    //     alert('successfully added user with username: ' + username);
-    //   });
-
+    })
+    // if (user) {
+    //     console.log(user.uid)
+    //     const doc = db1.collection('users').doc(user.uid);
+    //     doc.get().then((obj) => {
+    //         if (!obj.exists) {
+    //             console.log('no doc exists') 
+    //         }
+    //         else {
+    //             document.getElementById("firstName").innerHTML = obj.data().firstName;
+    //             document.getElementById("lastName").innerHTML = obj.data().lastName;
+    //             document.getElementById("location").innerHTML = obj.data().location;
+    //             document.getElementById("favoriteGenre").innerHTML = obj.data().favoriteGenre;
+    //             document.getElementById("biography").innerHTML = obj.data().biography;
+    //         }
+    //     })
+    // }
+    // else {
+    //     const url = window.location.href;
+    //     let username = url.substr(url.lastIndexOf("/") + 1);
+    //     const doc = db1.collection('users').where('username', '==', username);
+    //     doc.get().then((obj) => {
+    //         console.log(obj.data())
+    //         document.getElementById("firstName").innerHTML = obj.data().firstName;
+    //         document.getElementById("lastName").innerHTML = obj.data().lastName;
+    //         document.getElementById("location").innerHTML = obj.data().location;
+    //         document.getElementById("favoriteGenre").innerHTML = obj.data().favoriteGenre;
+    //         document.getElementById("biography").innerHTML = obj.data().biography;            
+    //     })
+    // }
 })
